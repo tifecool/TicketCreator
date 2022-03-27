c510bc9c201ad6e474df4cbaef7a6487060a474c
tifecool <tifebakre@yahoo.co.uk>
2022-03-27 10:08:59 +0100

    Implementation of UUID and Name Test, and saving image.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,11 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Test {
 
@@ -14,16 +17,39 @@ public class Test {
 
             Graphics backgroundImageGraphics = backgroundImage.getBackgroundImage().getGraphics();
 
-            backgroundImageGraphics.drawImage(qrCode.getQrCodeImage(), backgroundImage.getCentreWidth()- qrCode.getCentreWidth(), backgroundImage.getCentreHeight()- qrCode.getCentreHeight(), null);
+            backgroundImageGraphics.drawImage(qrCode.getQrCodeImage(), backgroundImage.getCentreWidth() - qrCode.getCentreWidth(), backgroundImage.getCentreHeight() - qrCode.getCentreHeight(), null);
+            Font font = new Font("Serif", Font.PLAIN, 12);
+
+            JLabel name = new JLabel("NAme Man ME",SwingConstants.CENTER);
+            JLabel uuid = new JLabel("UUIDDDDSDIDSNDNSDNSIDNDNSID", SwingConstants.CENTER);
+            uuid.setForeground(Color.BLACK);
+            name.setForeground(Color.BLACK);
+            //uuid.setLocation(backgroundImage.getCentreWidth() - uuid.getWidth(), (backgroundImage.getBackgroundImage().getHeight() * 9) / 10);
 
             JLabel picLabel = new JLabel(new ImageIcon(backgroundImage.getBackgroundImage()));
-            JPanel jPanel = new JPanel();
-            jPanel.add(picLabel);
+            picLabel.setLayout(new BorderLayout());
+            //JPanel jPanel = new JPanel();
+            uuid.setBorder(new EmptyBorder(0,0,backgroundImage.getBackgroundImage().getHeight()/10,0));
+            name.setBorder(new EmptyBorder(backgroundImage.getBackgroundImage().getHeight()/10,0,0,0));
+
+            picLabel.add(name,BorderLayout.NORTH);
+            picLabel.add(uuid, BorderLayout.SOUTH);
+            //jPanel.setLayout(null);
+            //jPanel.add(picLabel);
+            //jPanel.add(uuid);
 
             JFrame f = new JFrame();
+            f.setLayout(new GridBagLayout()); //Essential
+            f.add(picLabel);
             f.setSize(new Dimension(backgroundImage.getBackgroundImage().getWidth(), backgroundImage.getBackgroundImage().getHeight()));
-            f.add(jPanel);
+            f.pack();
+            f.setDefaultCloseOperation(EXIT_ON_CLOSE);
             f.setVisible(true);
+
+            Container c = f.getContentPane();
+            BufferedImage im = new BufferedImage(c.getWidth(), c.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            c.paint(im.getGraphics());
+            ImageIO.write(im, "PNG", new File("C:\\Users\\tifeb\\Desktop\\lol.png"));
 
         } catch (Exception ignored) {//Do proper Error checking
 
